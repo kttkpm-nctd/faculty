d2a654f068ef7e26cc5c6f34ac9bc2b5beb2b0ea
datma <datma113112111@gmail.com>
2021-10-21 14:44:31 +0700

    Initial commit

---
+++ src/test/java/iuh/nctd/faculty/FacultyApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.nctd.faculty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
